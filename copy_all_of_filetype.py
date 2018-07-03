43703c1e304acd7449b1d4a10aa0c7e2e18931a3
Devyzr <daniel.e.verdin@gmail.com>
2018-07-03 13:42:26 -0500

    Made simpler, also a class now.

--- copy_all_of_filetype.py
+++ copy_all_of_filetype.py
@@ -8,28 +8,34 @@ from os import sep, mkdir
 from os.path import isdir, isfile
 from shutil import copyfile
 
-files = get_all_files()
-dest_folder = 'copied_files'
-file_ext = ''
 
-if not isdir(dest_folder):
-    mkdir(dest_folder)
+class CopyAllFiles:
 
-for file in files:
-    split_f = file.split(sep)
-    filename = split_f[-1]
-    if file_ext in filename:
-        split_filename = filename.split('.')
-        filename = '.'.join(split_filename[:-2])
-        new_file_loc = ''.join([dest_folder, sep, filename, file_ext])
-        file_exists = isfile(new_file_loc)
-        if file_exists:
-            file_no = 1
-            while file_exists:
-                new_file_loc = ''.join([dest_folder, sep, filename, '_',
-                                        str(file_no), file_ext])
-                file_no += 1
-                file_exists = isfile(new_file_loc)
+    def massCopy(self, file_ext, dest_folder):
+        files = get_all_files('.', file_ext)
 
-        print('Copying %s' % new_file_loc)
-        copied_file = copyfile(file, new_file_loc)
+        if not isdir(dest_folder):
+            mkdir(dest_folder)
+
+        for file in files:
+            # Get the filename
+            split_f = file.split(sep)
+            filename = split_f[-1]
+            new_file_loc = ''.join([dest_folder, sep, filename])
+            file_exists = isfile(new_file_loc)
+            if file_exists:
+                file_no = 1
+                while file_exists:
+                    new_file_loc = ''.join([dest_folder, sep, filename, '_',
+                                            str(file_no)])
+                    file_no += 1
+                    file_exists = isfile(new_file_loc)
+
+            copied_file = copyfile(file, new_file_loc)
+            print('Copied %s' % copied_file)
+
+
+if __name__ == "__main__":
+    file_ext = ''
+    dest_folder = 'copied_files'
+    CopyAllFiles.massCopy(file_ext, dest_folder)
